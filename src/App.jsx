06b59f419d3c6f40c3fdb1765fe16ba4b5ad600d
Leah Irwin <irwin.leah@outlook.com>
2025-03-31 12:36:55 -0400

    src/components and src/services added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
+// src/App.jsx
+
 const App = () => {
-  const todo = { text: 'A brand new task', done: true };
-
-  return (
-    <>
-    <h1>JavaScript in JSX</h1>
-    <p>{todo.text}</p>
-
-    <h2>Conditional rendering</h2>
-    <p>{todo.done ? `Task Completed - ${todo.text}` : todo.text }</p>
-    </>
-  );
+  return <h1>Hello world!</h1>;
 };
 
 export default App;
