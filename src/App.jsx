baabe12c041abb598b60b0e74eaa6db09fd49480
Leah Irwin <irwin.leah@outlook.com>
2025-03-31 12:59:45 -0400

    setting up videos state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 // src/App.jsx
+import { use } from "react";
+import { useState, useEffect } from "react";
+import * as videoService from './services/videoService';
+
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const featuredVideos = async () => {
+      try {
+        const featuredVideos = await videoService.index();
+        if (featuredVideos.err) {
+          throw new Error(featuredVideos.err);
+        }
+        setVideos(featuredVideos);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    featuredVideos();
+  }, []);
+
   return <h1>Hello world!</h1>;
 };
 
