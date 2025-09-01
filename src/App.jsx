73494bdfb16016e71fe176706f6166ebf18f60a4
Leah Irwin <irwin.leah@outlook.com>
2025-09-01 13:51:43 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
 // src/App.jsx
 
-import { useState, useEffect } from "react";
-import * as videoService from './services/videoService';
-
-import VideoList from "./components/VideoList/VideoList";
-
-
-
-
 const App = () => {
-  const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    const featuredVideos = async () => {
-      try {
-        const featuredVideos = await videoService.index();
-        if (featuredVideos.err) {
-          throw new Error(featuredVideos.err);
-        }
-        setVideos(featuredVideos);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    featuredVideos();
-  }, []);
 
   return (
-    <>
-      <VideoList videos={videos} />
-    </>
+    <h1>Hello, world!</h1>
   );
 };
 
