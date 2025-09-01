b0897e939873706eb92e0876df42648388bc54c3
Leah Irwin <irwin.leah@outlook.com>
2025-09-01 14:31:51 -0400

    updated conditional rendering in loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-// src/App.jsx
-
 const App = () => {
-  const todo = { text: 'A brand new task', done: false };
-
+  const todo = { text: 'A brand new task', done: true }
   const todos = [
     {text: 'Learn JavaScript', done: true},
-    {text: 'Learn JSX', done: false},
+    {text: 'Learn JSX', done: true},
     {text: 'Learn HTML', done: true},
     {text: 'Learn CSS', done: true},
     {text: 'Master React', done: false},
   ];
-
-  const todoList = todos.map((todo, index) =>
-    <li key={index}>{todo.text}</li>
-  )
 
   return (
     <>
@@ -21,12 +14,27 @@ const App = () => {
       <p>{todo.text}</p>
 
       <h2>Conditional Rendering</h2>
-      <p>{todo.done ? `Task Completed - ${todo.text}` : todo.text}</p>
+      <p>{todo.done ? `Task Completed - ${todo.text}` : todo.text }</p>
 
       <h2>Looping with JSX</h2>
-      <ul>{todoList}</ul>
+      <ul>
+        {todos.map((todo, index) =>
+          <li key={index}>
+            {todo.text}
+          </li>
+        )}
+      </ul>
+
+      <h2>Looping and Conditional Rendering</h2>
+      <ul>
+        {todos.map((todo, index) => 
+          <li key={index}>
+            {todo.done ? `Task Completed - ${todo.text}` : todo.text}
+          </li>
+        )}
+      </ul>
     </>
   );
-};
+}
 
-export default App;
+export default App
