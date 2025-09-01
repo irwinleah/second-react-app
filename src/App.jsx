34129238cd888dd8ec4faba43f8a2047c134a6b7
Leah Irwin <irwin.leah@outlook.com>
2025-09-01 14:14:26 -0400

    added todos variable array

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@
 
 const App = () => {
   const todo = { text: 'A brand new task', done: false };
+
+  const todos = [
+    {text: 'Learn JavaScript', done: true},
+    {text: 'Learn JSX', done: false},
+    {text: 'Learn HTML', done: true},
+    {text: 'Learn CSS', done: true},
+    {text: 'Master React', done: false},
+  ];
+  
   return (
     <>
       <h1>JavaScript in JSX</h1>
